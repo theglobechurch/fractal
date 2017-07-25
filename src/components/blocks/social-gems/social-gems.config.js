2bd1f801b8cdcaa9b482051d035942c0c6e39444
James Doc <jamesdoc@gmail.com>
2017-07-25 17:57:33 +0100

    Social gems

---
+++ src/components/blocks/social-gems/social-gems.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: 'Social gems',
+  label: 'Social gems',
+  status: 'wip',
+  context: {
+    services: [
+      {
+        href: 'https://twitter.com/theglobechurch',
+        service: 'twitter'
+      },
+      {
+        href: 'https://facebook.com/theglobechurch',
+        service: 'facebook'
+      },
+      {
+        href: 'https://instagram.com/theglobechurch',
+        service: 'instagram'
+      }
+    ]
+  }
+}
