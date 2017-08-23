32c36f193adcd2f78b8e2b9571c946d8f7a3fcfb
James Doc <jamesdoc@gmail.com>
2017-08-23 12:51:03 +0100

    Add series grid

---
+++ src/components/blocks/series-list/series-list.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: 'Series list',
+  label: 'Series list',
+  status: 'wip',
+  context: {
+    series: [
+      {
+        img: '/assets/img/1cor-square.jpg',
+        title: '1 Corinthians: (un)Impressive',
+        dateRange: 'September - December 2017'
+      },
+      {
+        img: '/assets/img/phi-square.jpg',
+        title: 'Philemon: For Loves Sake',
+        dateRange: 'July - August 2017'
+      },
+      {
+        img: '/assets/img/habakkuk-square.jpg',
+        title: 'Habakkuk: Worth Waiting For',
+        dateRange: 'April - May 2017'
+      },
+      {
+        img: '/assets/img/ruth-square.jpg',
+        title: 'Ruth: He Noticed Me',
+        dateRange: 'May - June 2016'
+      }
+    ]
+  }
+};
