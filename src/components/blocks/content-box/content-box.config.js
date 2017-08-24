6b2f9bf28132f966e9fe55d9b92cd59caab5036f
James Doc <jamesdoc@gmail.com>
2017-08-24 17:58:32 +0100

    context box with a text focus

--- src/components/blocks/content-box/content-box.config.js
+++ src/components/blocks/content-box/content-box.config.js
@@ -52,6 +52,34 @@ module.exports = {
         list: true,
         actionBtn: 'Get directions'
       }
+    },
+    {
+      name: 'with all the content',
+      label: 'With all the content',
+      context: {
+        body: [
+          'The Bible uses several pictures of the church to illustrate the idea of membership. For example, the parts of a body, bricks in a building, soldiers in an army and perhaps most powerfully members of one family. These pictures all contain the idea of belonging, it is much more than attending. Church isn’t supposed to be a spectator sport. It is much more of a family who are all involved in different ways to make the family work properly.',
+          'Therefore we believe that anyone who is trusting Jesus and has decided to make Globe church their home is a member of the family.',
+          'We then encourage those who are members at the Globe Church to express that membership publicly by becoming part of our formal membership. It can be really easy to float around various churches in London and we think that is unhelpful. Membership expresses a commitment to one local church family. It is also legally how we are constituted. There are regular church members meetings and any changes in church leadership need to be agreed by a vote of the membership.',
+          'Becoming a member means simply filling in a form that contains these four ideas:',
+          '<ol><li>I commit (this is my church family)</li><li>I need (I can’t go it alone)</li><li>I entrust (I want this church to look out for me)</li><li>I give (I want to be involved in the mission)</li></ol>',
+          'Membership forms can be found at the back of church. If you are interested then fill in a form or chat to one of the elders.'
+        ],
+        className: 'c-content-box',
+        header: {
+          img: {
+            2560: '/assets/img/templates/series-and-resources/img/series-and-resources-banner-2560.jpg',
+            1920: '/assets/img/templates/series-and-resources/img/series-and-resources-banner-1920.jpg',
+            1280: '/assets/img/templates/series-and-resources/img/series-and-resources-banner-1280.jpg',
+            960: '/assets/img/templates/series-and-resources/img/series-and-resources-banner-960.jpg',
+            640: '/assets/img/templates/series-and-resources/img/series-and-resources-banner-640.jpg',
+            320: '/assets/img/templates/series-and-resources/img/series-and-resources-banner-320.jpg'
+          },
+          title: 'Membership',
+          subtitle: 'Committing, needing, entrusting, giving'
+        },
+        textFocus: true
+      }
     }
   ]
 };
