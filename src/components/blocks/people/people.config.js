c61fccfd0be61a2540367bd7cad7d0d62bb67c57
James Doc <jamesdoc@gmail.com>
2017-09-22 16:39:22 +0100

    People block

---
+++ src/components/blocks/people/people.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: 'People',
+  context: {
+    people: [
+      {
+        name: 'Jonty Allcock',
+        title: 'Senior Pastor',
+        img: '/assets/img/blocks/people/img/jonty-square.jpg'
+      },
+      {
+        name: 'Alice Cudmore',
+        title: 'Ministy Trainee',
+        img: '/assets/img/blocks/people/img/alice-square.jpg'
+      },
+      {
+        name: 'Phil Tinker',
+        title: 'Assistant Pastor',
+        img: '/assets/img/blocks/people/img/phil-square.jpg'
+      }
+    ]
+  }
+};
