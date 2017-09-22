5a0c0cac77362bde703ff3801f1ce9c0ed535112
James Doc <jamesdoc@gmail.com>
2017-09-22 13:38:48 +0100

    Fix content boxes and update about preview

--- src/components/templates/about/about.config.js
+++ src/components/templates/about/about.config.js
@@ -21,44 +21,53 @@ module.exports = {
     },
     descBox: [
       {
-        title: 'The Globe Church is all about Jesus…',
-        body: ['We’re a church that proclaims the good news of Jesus Christ. Whether you are passing through London for a weekend, or living locally in Southwark when you come to The Globe Church you’re going to hear the Bible preached.'],
-        img: {
-          2560: '/assets/img/blocks/vision-box/img/jonty-2560.jpg',
-          1920: '/assets/img/blocks/vision-box/img/jonty-1920.jpg',
-          1280: '/assets/img/blocks/vision-box/img/jonty-1280.jpg',
-          960: '/assets/img/blocks/vision-box/img/jonty-960.jpg',
-          640: '/assets/img/blocks/vision-box/img/jonty-640.jpg',
-          320: '/assets/img/blocks/vision-box/img/jonty-320.jpg'
-        },
-        theme: 'sockeye'
+        body: [
+          'We are seriously excited about Jesus. His extraordinary life has changed the course of human history.',
+          'In the Bible we find that in the beginning, we were created by God to enjoy life forever with him. But we chose to go it alone. We turned our backs and acted as if we were king.'
+        ],
+        className: 'c-content-box',
+        textFocus: true,
+        header: {
+          title: 'The Globe Church is all about Jesus…',
+          img: {
+            2560: '/assets/img/blocks/vision-box/img/jonty-2560.jpg',
+            1920: '/assets/img/blocks/vision-box/img/jonty-1920.jpg',
+            1280: '/assets/img/blocks/vision-box/img/jonty-1280.jpg',
+            960: '/assets/img/blocks/vision-box/img/jonty-960.jpg',
+            640: '/assets/img/blocks/vision-box/img/jonty-640.jpg',
+            320: '/assets/img/blocks/vision-box/img/jonty-320.jpg'
+          }
+        }
       },
       {
-        title: 'Made up of all sorts of people',
         body: [''],
-        img: {
-          2560: '/assets/img/blocks/vision-box/img/people-2560.jpg',
-          1920: '/assets/img/blocks/vision-box/img/people-1920.jpg',
-          1280: '/assets/img/blocks/vision-box/img/people-1280.jpg',
-          960: '/assets/img/blocks/vision-box/img/people-960.jpg',
-          640: '/assets/img/blocks/vision-box/img/people-640.jpg',
-          320: '/assets/img/blocks/vision-box/img/people-320.jpg'
-        },
-        theme: 'gold-braid',
-        flip: true
+        textFocus: true,
+        header: {
+          title: 'Made up of all sorts of people',
+          img: {
+            2560: '/assets/img/blocks/vision-box/img/people-2560.jpg',
+            1920: '/assets/img/blocks/vision-box/img/people-1920.jpg',
+            1280: '/assets/img/blocks/vision-box/img/people-1280.jpg',
+            960: '/assets/img/blocks/vision-box/img/people-960.jpg',
+            640: '/assets/img/blocks/vision-box/img/people-640.jpg',
+            320: '/assets/img/blocks/vision-box/img/people-320.jpg'
+          }
+        }
       },
       {
-        title: 'Involved in the greatest mission',
         body: ['Borough in London is where we met as a church family, and we are called to share the good news of Jesus Christ to the people there…'],
-        img: {
-          2560: '/assets/img/blocks/vision-box/img/mission-2560.jpg',
-          1920: '/assets/img/blocks/vision-box/img/mission-1920.jpg',
-          1280: '/assets/img/blocks/vision-box/img/mission-1280.jpg',
-          960: '/assets/img/blocks/vision-box/img/mission-960.jpg',
-          640: '/assets/img/blocks/vision-box/img/mission-640.jpg',
-          320: '/assets/img/blocks/vision-box/img/mission-320.jpg'
-        },
-        theme: 'lark-green'
+        textFocus: true,
+        header: {
+          title: 'Involved in the greatest mission',
+          img: {
+            2560: '/assets/img/blocks/vision-box/img/mission-2560.jpg',
+            1920: '/assets/img/blocks/vision-box/img/mission-1920.jpg',
+            1280: '/assets/img/blocks/vision-box/img/mission-1280.jpg',
+            960: '/assets/img/blocks/vision-box/img/mission-960.jpg',
+            640: '/assets/img/blocks/vision-box/img/mission-640.jpg',
+            320: '/assets/img/blocks/vision-box/img/mission-320.jpg'
+          }
+        }
       }
     ],
     promobox: {
