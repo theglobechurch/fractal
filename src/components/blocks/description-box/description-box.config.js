a57ead809d01f55aa5f9a3abc2fef0e4e34794a6
James Doc <jamesdoc@gmail.com>
2017-09-22 12:48:45 +0100

    Adjust description-box for staff profile
    Watch out for breaking changes to `.img-square` in `_images.scss`
    
    Also updated the sermon-list styling to remove left/right padding

--- src/components/blocks/description-box/description-box.config.js
+++ src/components/blocks/description-box/description-box.config.js
@@ -58,10 +58,38 @@ module.exports = {
         theme: 'lark-green',
         flip: false,
         series: {
+          imgVariant: 'square',
           meta: [
             'July – August 2017',
             '5 recordings'
-          ]
+          ],
+          list: true
+        }
+      }
+    },
+    {
+      name: 'person-profile',
+      label: 'Person profile',
+      context: {
+        title: 'Jonty Allcock',
+        body: [
+          'I love Jesus because he first loved me. When I was far away from him, he gave his life to save me. That is a staggering fact that humbles me and at the same time lifts up my head.',
+          'My journey began in Southampton. I grew up in a home that was (not perfect but) full of Jesus. I left home to study Chemistry at Oxford and was confronted with a challenging question: Had I been brainwashed, or did I really believe this? It was a struggle. I found university very hard. But as I looked closely into the Bible, I came to the deep conviction that it is true. My experience of living the Christian life has shown me that struggling and growing seem to go hand in hand.',
+          'On leaving university, I married Linda, and together we moved to Enfield in North London. For the next seven years, I was involved in and trained in local church ministry. I was then involved in planting a new church in Enfield where I was the pastor for seven years.',
+          'I have now moved into Central London (with Linda and my three boys) to continue the journey of faith here. I am far from perfect, but Jesus is a patient and wonderful king. It is my great honour to serve him at The Globe Church.'
+        ],
+        img: {
+          2560: '/assets/img/phi-2560.jpg',
+          1920: '/assets/img/phi-1920.jpg',
+          1280: '/assets/img/phi-1280.jpg',
+          960: '/assets/img/phi-960.jpg',
+          640: '/assets/img/phi-640.jpg',
+          320: '/assets/img/phi-320.jpg'
+        },
+        theme: 'lark-green',
+        flip: false,
+        series: {
+          imgVariant: 'circle'
         }
       }
     }
