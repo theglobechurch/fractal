6368de83e04b13aa977a92102735421148b15503
James Doc <jamesdoc@gmail.com>
2018-09-20 17:43:54 +0100

    Value block

---
+++ src/components/blocks/value-block/value-block.config.js
@@ -0,0 +1,76 @@
+module.exports = {
+  name: 'Value block',
+  label: 'Value block',
+  context: {},
+  variants: [
+    {
+      name: 'default',
+      label: "Relational",
+      context: {
+        modifier: 'relational',
+        img: {
+          2560: '/assets/img/blocks/value-block/img/relational-2560.jpg',
+          1920: '/assets/img/blocks/value-block/img/relational-1920.jpg',
+          1280: '/assets/img/blocks/value-block/img/relational-1280.jpg',
+          960: '/assets/img/blocks/value-block/img/relational-960.jpg',
+          640: '/assets/img/blocks/value-block/img/relational-640.jpg',
+          320: '/assets/img/blocks/value-block/img/relational-320.jpg'
+        },
+        title: '1. Relational',
+        tag: 'Embracing and overcoming city life to be family',
+        detail: 'We love being a church in the heart of London. And while we might live in different parts of the city, we\'re committed to living life as family- caring for each other, building relationships, and helping each other live for Christ.',
+      }
+    },
+    {
+      name: 'Discipling',
+      context: {
+        modifier: 'discipling',
+        img: {
+          2560: '/assets/img/blocks/value-block/img/discipling-2560.jpg',
+          1920: '/assets/img/blocks/value-block/img/discipling-1920.jpg',
+          1280: '/assets/img/blocks/value-block/img/discipling-1280.jpg',
+          960: '/assets/img/blocks/value-block/img/discipling-960.jpg',
+          640: '/assets/img/blocks/value-block/img/discipling-640.jpg',
+          320: '/assets/img/blocks/value-block/img/discipling-320.jpg'
+        },
+        title: '2. Discipling',
+        tag: 'Equipping a generation to live all of life for Jesus',
+        detail: 'There are two things that we talk a lot about at The Globe Church: Christ and the Bible. We\'re convinced that by going deeper into who Christ is and what he\'s done as we discover in the Bible we can grow together and be able to live for him.',
+      }
+    },
+    {
+      name: 'Missional',
+      context: {
+        modifier: 'missional',
+        img: {
+          2560: '/assets/img/blocks/value-block/img/missional-2560.jpg',
+          1920: '/assets/img/blocks/value-block/img/missional-1920.jpg',
+          1280: '/assets/img/blocks/value-block/img/missional-1280.jpg',
+          960: '/assets/img/blocks/value-block/img/missional-960.jpg',
+          640: '/assets/img/blocks/value-block/img/missional-640.jpg',
+          320: '/assets/img/blocks/value-block/img/missional-320.jpg'
+        },
+        title: '3. Missional',
+        tag: 'Confidently living to make Christ known to all people.',
+        detail: 'The good news about Jesus is the only thing that can change people\'s lives and bring them life with God. So, we\'re passionate about telling people about Jesus through our everyday lives and together as a church.',
+      }
+    },
+    {
+      name: 'Generous',
+      context: {
+        modifier: 'generous',
+        img: {
+          2560: '/assets/img/blocks/value-block/img/generous-2560.jpg',
+          1920: '/assets/img/blocks/value-block/img/generous-1920.jpg',
+          1280: '/assets/img/blocks/value-block/img/generous-1280.jpg',
+          960: '/assets/img/blocks/value-block/img/generous-960.jpg',
+          640: '/assets/img/blocks/value-block/img/generous-640.jpg',
+          320: '/assets/img/blocks/value-block/img/generous-320.jpg'
+        },
+        title: '4. Generous',
+        tag: 'Committed to sharing resources to be a blessing to others.',
+        detail: 'We don\'t exist to build a name for ourselves, but to be part of God\'s wider church to make Christ known. God\'s generosity in Christ drives us to want to be generous in prayer, with our people, and any other resources that can serve the wider church.',
+      }
+    }
+  ]
+};
---
+++ src/components/templates/values/values.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  status: 'wip',
+  context: {
+    banner: {
+      className: 'c-banner',
+      img: {
+        2560: '/assets/img/globals/banner/img/southbank-2560.jpg',
+        1920: '/assets/img/globals/banner/img/southbank-1920.jpg',
+        1280: '/assets/img/globals/banner/img/southbank-1280.jpg',
+        960: '/assets/img/globals/banner/img/southbank-960.jpg',
+        640: '/assets/img/globals/banner/img/southbank-640.jpg',
+        320: '/assets/img/globals/banner/img/southbank-320.jpg'
+      },
+      pretitle: 'What matters in The Globe Church',
+      title: 'Values'
+    }
+  }
+};
