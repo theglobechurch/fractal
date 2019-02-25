1272518b90fc397ec45c15223c2a13376b0aeedc
James Doc <jamesdoc@gmail.com>
2019-02-25 11:38:14 +0000

    Add tag styles

---
+++ src/components/02-units/tags/tags.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  context: {
+    tags: [
+      {
+        title: 'Danger',
+        type: 'danger'
+      },
+      {
+        title: 'Alert',
+        type: 'alert'
+      },
+      {
+        title: 'Success',
+        type: 'success'
+      },
+      {
+        title: 'Normal',
+        type: 'normal'
+      },
+    ]
+  }
+}
