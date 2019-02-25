83346f2828e52ead54bdf7356be5610d619e3241
James Doc <jamesdoc@gmail.com>
2019-02-25 12:16:55 +0000

    Basic event list

---
+++ src/components/blocks/event-list/event-list.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  context: {
+    events: [
+      {
+        dt: '2018-10-10T19:00:00',
+        title: 'Globe Focus',
+        location: 'Willcox House, 140–148 Borough High Street',
+        moreInfo: true
+      },
+      {
+        dt: '2018-10-11T19:00:00',
+        title: 'Globe Focus',
+        location: 'Willcox House, 140–148 Borough High Street',
+        moreInfo: true
+      },
+      {
+        dt: '2018-10-12T08:15:00',
+        title: 'Men\'s Breakfast One21 Group',
+        location: 'Costa Coffee, Liverpool Street',
+        moreInfo: false
+      },
+      {
+        dt: '2018-10-13T14:45:00',
+        title: 'London Women\'s Convention',
+        location: 'The Emmanuel Centre, Westminster',
+        moreInfo: true,
+        tag: 'Special event'
+      },
+      {
+        dt: '2018-10-14T10:30:00',
+        title: 'Globe Extra',
+        location: 'Willcox House, 140–148 Borough High Street',
+        moreInfo: true,
+        tag: 'Monthly event'
+      },
+      {
+        dt: '2018-10-14T16:30:00',
+        title: 'Church Service',
+        location: 'New Hunt\'s House, Guy\'s Campus',
+        moreInfo: true
+      }
+    ]
+  }
+}
