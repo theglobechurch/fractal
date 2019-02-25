c2b14556a1f6da523cfb20c5a6ec48d7ec4914ff
James Doc <jamesdoc@gmail.com>
2019-02-25 23:14:11 +0000

    section links

---
+++ src/components/blocks/event-section-link/event-section-link.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  context: {
+    blocks: [
+      {
+        title: 'Sundays',
+        subtitle: 'Church Service',
+        img: {
+          2560: '/assets/img/blocks/value-block/img/relational-2560.jpg',
+          1920: '/assets/img/blocks/value-block/img/relational-1920.jpg',
+          1280: '/assets/img/blocks/value-block/img/relational-1280.jpg',
+          960: '/assets/img/blocks/value-block/img/relational-960.jpg',
+          640: '/assets/img/blocks/value-block/img/relational-640.jpg',
+          320: '/assets/img/blocks/value-block/img/relational-320.jpg'
+        }
+      },
+      {
+        title: 'Globe Focus',
+        subtitle: 'Small Group Bible Studies',
+        img: {
+          2560: '/assets/img/blocks/value-block/img/discipling-2560.jpg',
+          1920: '/assets/img/blocks/value-block/img/discipling-1920.jpg',
+          1280: '/assets/img/blocks/value-block/img/discipling-1280.jpg',
+          960: '/assets/img/blocks/value-block/img/discipling-960.jpg',
+          640: '/assets/img/blocks/value-block/img/discipling-640.jpg',
+          320: '/assets/img/blocks/value-block/img/discipling-320.jpg'
+        }
+      },
+      {
+        title: 'Globe Central',
+        subtitle: 'Prayer Gathering',
+        img: {
+          2560: '/assets/img/blocks/value-block/img/missional-2560.jpg',
+          1920: '/assets/img/blocks/value-block/img/missional-1920.jpg',
+          1280: '/assets/img/blocks/value-block/img/missional-1280.jpg',
+          960: '/assets/img/blocks/value-block/img/missional-960.jpg',
+          640: '/assets/img/blocks/value-block/img/missional-640.jpg',
+          320: '/assets/img/blocks/value-block/img/missional-320.jpg'
+        }
+      }
+    ],
+    additionalNav: [
+      'Next Seven Days',
+      'Further Ahead'
+    ]
+  }
+}
