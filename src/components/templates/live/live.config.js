ee627b0b732a873e847a97af2320c1c54f75e1d9
James Doc <hi@jamesdoc.com>
2020-03-20 19:40:17 +0000

    Fix: Alignment of Livestream banner;

---
+++ src/components/templates/live/live.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  status: 'wip',
+  context: {
+    banner: {
+      className: 'c-banner',
+      variant: ['video'],
+      video: 'https://www.youtube.com/embed/wIWt0kTtp3U',
+      title: 'About Globe Church',
+      subtitle: 'The Globe Church is all about sharing the good news of Jesus Christ to those living and working on the Southbank of London.',
+    },
+    intro: {
+      className: 'c-content-box',
+      title: 'We\'re a church made up of many stories…',
+      subtitle: 'But, if we were to pick three words to define us, they would be Jesus, people and mission. That’s what we are about.',
+    },
+
+    promobox: {
+      className: 'c-promo-box',
+      href: '#',
+      title: 'Church leadership',
+      cta: 'Find out more',
+      img: {
+        2560: '/assets/img/phi-2560.jpg',
+        1920: '/assets/img/phi-1920.jpg',
+        1280: '/assets/img/phi-1280.jpg',
+        960: '/assets/img/phi-960.jpg',
+        640: '/assets/img/phi-640.jpg',
+        320: '/assets/img/phi-320.jpg',
+      },
+    },
+  },
+};
