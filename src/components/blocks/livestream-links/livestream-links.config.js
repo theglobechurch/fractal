478353e43145138aec859f717741992cbd0a24c5
James Doc <hi@jamesdoc.com>
2020-03-24 20:34:11 +0000

    Previous streams preview

---
+++ src/components/blocks/livestream-links/livestream-links.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  context: {
+    title: 'Previous streams',
+    streams: [
+      {
+        id: 'ra8rwS6r2Vo',
+        date: 'Tuesday, 24 March 2020 - 08:25',
+      },
+      {
+        id: 'B7M4QpEPwDc',
+        date: 'Monday, 23 March 2020 - 08:25',
+      },
+      {
+        id: '3AUGRNks8d4',
+        date: 'Sunday, 22 March 2020 - 16:30',
+      },
+      {
+        id: 'dQl4izxPeNU',
+        date: 'Friday, 21 March 2020 - 08:25',
+      },
+    ],
+  }
+}
